8231a74b070067b87b68f0fe03748a282ef0edf0
bharathan <bharathanbk3@gmail.com>
2025-05-27 00:06:38 +0530

    Functions

--- function.js
+++ function.js
@@ -1,2 +1,18 @@
-a="hello world "
-console.log(a)
+function abc(){
+    console.log("hello.....")
+}
+abc()
+function myname(){
+    console.log("BharathanK")
+}
+myname()
+function mobile(name){
+    if(name=="OPPO"){
+        console.log("this is OPPO phone");
+    } else if(name.toLowercase()=="Vivo"){
+        console.log("this is Vivo ");
+    }else{
+        console.log("You are not selected in any mobile")
+    }
+}
+mobile("VIVO");
