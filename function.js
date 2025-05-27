aad9a96b2da2021640dd771b2ca9c9fba6503dda
bharathan <bharathanbk3@gmail.com>
2025-05-27 22:29:27 +0530

    basic program

--- function.js
+++ function.js
@@ -1,19 +1,63 @@
-function abc(){
-    console.log("hello.....")
+// function abc(){
+//     console.log("hello.....")
+// }
+// abc()
+// function myname(){
+//     console.log("BharathanK")
+// }
+// myname()
+// function mobile(name){
+//     if(name=="OPPO"){
+//         console.log("this is OPPO phone");
+//     } else if(name=="Vivo"){
+//         console.log("this is Vivo ");
+//     }else{
+//         console.log("You are not selected in any mobile")
+//     }
+// }
+// mobile("VIVO");
+// //if else exerise 
+// //Q1
+// function traffic(color){
+//     if(color=="Red"){
+//         console.log("STOP")
+//     }
+//     else if(color=="YELLOW"){
+//         console.log("GetReady")
+//     }
+//     else if(color=="GREEN"){
+//         console.log("GO")
+//     }
+// }
+// traffic("GREEN")
+
+// //Q2
+// var score=76
+// if(score <= 50){
+//     console.log("You need to improve")
+// }
+// else if(score >= 50 && score<=70){
+// console.log("Good Job")
+// }
+// else if(score >= 70){
+//     console.log("Excellent performance")
+// }
+// Is even or odd number
+// var score=77
+// if(score % 2 == 0){
+//     console.log("This is Even number")
+// }
+// else{
+// console.log("ODD number")
+// }
+
+// var score = "a";
+// if (score == "a" || score == "e" || score == "i" || score == "o" || score == "u") {
+//     console.log("This vowel");
+// } else {
+//     console.log("This is consonant");
+// }
+
+for(var i=0;i<10;i++){
+    console.log(i)
 }
-abc()
-function myname(){
-    console.log("BharathanK")
-}
-myname()
-function mobile(name){
-    if(name=="OPPO"){
-        console.log("this is OPPO phone");
-    } else if(name.toLowercase()=="Vivo"){
-        console.log("this is Vivo ");
-    }else{
-        console.log("You are not selected in any mobile")
-    }
-}
-mobile("VIVO");
-//this onemore time pull changes
